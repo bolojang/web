fce51754a0805821bdcb43738beea92f20921051
Fanda Rezabek <rezabek2009@gmail.com>
2012-11-15 13:55:25 +0000

    just to test

---
+++ application/views/testView.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of testView
+ *
+ * @author FandaMacBook
+ */
+class testView {
+  //put your code here
+}
+
+?>
